dfe011cd9924a07ed89c6c6b7e553f6709f09480
Anna An <arzumanyan.an@gmail.com>
2023-02-06 11:20:31 +0400

    working on Author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
-const Author = () => {
-    return (
-        <h1>Author</h1>
-    )
-}
+const Author = ({ author }) => (
+  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute left-0 right-0 -top-9">
+      <Image
+        unoptimized
+        alt={author.name}
+        width={100}
+        height={100}
+        className="align-middle rounded-full mx-auto"
+        src={author.photo.url}
+      />
+    </div>
+    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-white text-ls">{author.bio}</p>
+  </div>
+);
 
-export default Author
+export default Author;

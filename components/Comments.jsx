a03da886f9eff9130bce3fa8c3692c800648680a
Anna An <arzumanyan.an@gmail.com>
2023-02-07 17:09:46 +0400

    working on the Comment Form

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Comments = () => {
-    return (
-        <h1>Comments</h1>
-    )
-}
+  return <h1>Comments</h1>;
+};
 
-export default Comments
+export default Comments;
